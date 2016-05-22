15cb146ba9c52913abe62e93159b5e31137e25da
Grady Vickery <k.vickery@ymail.com>
2016-05-22 11:03:27 -0500

    Added array for content to allow paragraph separation in posts

--- README.md
+++ README.md
@@ -20,4 +20,5 @@ Project notes:
 
     Added new components to free up App Component. These include footer, and content components. 
     
-    Routing has not been added....HOME and ABOUT links exist but are not hooked up. Any reloads must be currently done via browser refresh.
+    Routing has not been added....HOME and ABOUT links exist but are not hooked up. Any reloads
+    must be currently done via browser refresh.
--- assets/app/content.component.ts
+++ assets/app/content.component.ts
@@ -4,7 +4,7 @@ export class BlogPost {
     id: number;
     heading: string;
     subheading: string;
-    content: string;
+    content: string[];
     author: string;
     datePosted: string;
 }
@@ -46,7 +46,10 @@ export class BlogPost {
         </div>  
      
         <div *ngIf="selectedBlogPost" class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1" id="post-body">
-            <p>{{selectedBlogPost.content}}</p>
+            <p class="tab">{{selectedBlogPost.content[0]}}</p>
+            <p class="tab">{{selectedBlogPost.content[1]}}</p>
+            <p class="tab">{{selectedBlogPost.content[2]}}</p>
+            <p class="tab">{{selectedBlogPost.content[3]}}</p>
         </div>
        
     `
@@ -83,7 +86,22 @@ var BLOGPOSTS: BlogPost[] = [
         "id": 1,
         "heading": 'An introductory post by a new developer lunging into the world of JS',
         "subheading": 'Side A, song one...AngularJS ',
-        "content": 'Start of a really short blog.',
+           "content": ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam placerat laoreet augue, eu fermentum enim ullamcorper non.',
+               'Maecenas lectus enim, dictum quis aliquam at, pulvinar sed lectus. Fusce aliquam lobortis dui, non hendrerit nisi facilisis ac. Nullam'+
+
+           'rutrum mattis ultricies. Donec condimentum sem nec nulla ultrices bibendum. Nullam volutpat dolor a molestie tincidunt. Fusce eget dolor viverra, varius nibh sit amet, sollicitudin lorem.' +
+        'Proin bibendum orci vel sem lacinia, eleifend suscipit dui lacinia. Curabitur consequat lacinia sapien, accumsan iaculis risus tempor elementum. Nunc nibh arcu, tincidunt eu massa a, cursus dictum erat. Curabitur luctus felis ac euismod ultricies. Fusce congue, orci sed eleifend iaculis, libero elit aliquet ante, nec tempus elit ligula in augue. Donec aliquam, dolor nec venenatis egestas, nibh mi iaculis tellus, non cursus ante lectus et eros. Quisque feugiat ipsum nec euismod semper. Sed sed laoreet elit. Fusce nisl sapien, lacinia sit amet egestas vitae, ultrices in eros. Ut vitae massa aliquam, consectetur libero a, facilisis enim.' +
+            'Pellentesque sit amet congue sapien, dapibus ornare nisi. Duis at ex sagittis lorem dignissim sodales. Mauris congue sit amet erat vel.',
+
+           'Elementum. Morbi ac varius dui. Etiam non urna suscipit, commodo velit vitae, dictum purus. Nunc euismod libero sed maximus fermentum. Nullam aliquam nec enim quis ultrices. Ut pharetra aliquet tortor, a maximus libero laoreet ac. Quisque eu commodo enim, ut scelerisque elit. Duis placerat nulla nec luctus pretium. Sed vulputate nisi a lorem viverra, in gravida orci iaculis. Fusce quis rhoncus sapien. Praesent fermentum vehicula purus at vestibulum.',
+    'Duis sed enim sit amet lorem blandit sollicitudin suscipit at purus. Suspendisse urna lectus, gravida vitae vestibulum vel, imperdiet in nunc. In et venenatis mi. Vivamus in dolor consequat ex scelerisque pulvinar. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi quis mi maximus, tristique augue quis, venenatis nisi. Nulla luctus felis vitae orci gravida mattis. Ut dapibus imperdiet eros, a auctor diam interdum non. Vivamus at lorem enim.' +
+    'Quisque luctus efficitur augue, sed ullamcorper arcu mattis pharetra. Ut euismod dui vitae justo finibus, eget pretium tellus pulvinar. Praesent laoreet eleifend libero. Aenean eget dolor ante. Aenean sed pulvinar velit, a congue arcu. Sed ac fermentum arcu, quis laoreet neque. Ut sodales sem eros, sit amet eleifend odio bibendum at. Donec sagittis elit ut lectus pulvinar, id cursus ligula scelerisque. Sed in vulputate nulla. Nam quis risus ac nisi malesuada pellentesque. ' +
+    'Proin volutpat vitae est et posuere. Aliquam ac vehicula libero, scelerisque efficitur tortor. In commodo fermentum fermentum. Sed condimentum dolor non auctor laoreet. Cras consequat metus mi. Etiam egestas tortor non fringilla tristique. Duis iaculis ullamcorper fermentum. Sed nec sollicitudin elit. Praesent non efficitur leo. Phasellus ut nunc at dui commodo ultricies nec malesuada mauris. Curabitur faucibus nisl quis mauris mollis gravida. Etiam tempor ante lectus, vel hendrerit diam' +
+    'semper ac. Donec hendrerit augue eget malesuada pharetra. Suspendisse gravida eu leo pulvinar efficitur. Aliquam ut elementum lacus, quis bibendum quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.']
+
+
+
+
         "author": 'Grady Vickery',
         "datePosted": 'May 11,2016'
     },
@@ -91,7 +109,10 @@ var BLOGPOSTS: BlogPost[] = [
         "id": 2,
         "heading":'Title 2',
         "subheading": 'Subheading goes here',
-        "content": 'End of the start of a really short blog',
+        "content": [
+            'End of the start of a really short blog',
+            'iii      iiii           iiiii',
+            'More here too'],
         "author": 'Grady Vickery',
         "datePosted": 'May 12,2016'
     },
